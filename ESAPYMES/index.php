8e5f757afa2aa9211b2e2dbb751641a6209fb800
fabiansamus <samus0226@hotmail.com>
2017-07-19 09:53:58 -0400

    files

---
+++ ESAPYMES/index.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="shortcut icon" href="assets/ico/favicon.ico">
+
+        <title>Portada | ESA PYMES</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="assets/css/style.css" rel="stylesheet">
+        <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+        <script src="assets/js/modernizr.js"></script>
+    </head>
+
+    <body>
+        <div class="color-panel hidden-sm" style="background-color: none; padding: 5px; z-index: 9999; position: fixed; top: 100px; right: 0;">
+            <div class="icon-color">
+                <a target="_BLANK" href="http://facebook.com"><img src="http://desiweb.com.do/DesiWeb/img/service-icon/facebook.png" width="32" height="32"></a>
+                <br><br>
+                <a target="_BLANK" href="http://twitter.com"><img src="http://desiweb.com.do/DesiWeb/img/service-icon/twitter.png" width="32" height="32"></a>
+                <br><br>
+                <a target="_BLANK" href="http://instagram.com"><img src="http://www.iconarchive.com/download/i62741/designbolts/free-instagram/Active-Instagram-3.ico" width="32" height="32"></a>
+            </div>
+        </div>
+        <!-- Fixed navbar -->
+        <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+            
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <img src="assets/img/portada/logo-esapymes.png" width="150" style="margin-top: -10px;">
+                </div>
+                <div class="navbar-collapse collapse navbar-right">
+                    <ul class="nav navbar-nav" style="margin-top: 5px;">
+                        <li class="active"><a href="index.html">PORTADA</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">SERVICIOS <b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">CONTABLE Y FISCAL</a></li>
+                                <li><a href="#">AUDITORÍA</a></li>
+                                <li><a href="#">GESTIÓN HUMANA</a></li>
+                                <li><a href="#">TECNOLOGÍA</a></li>
+                                <li><a href="#">MERKETING Y REDES SOCIALES</a></li>
+                                <li><a href="#">LEGAL & LITIGIOS</a></li>
+                                <li><a href="#">CONTROL DE GESTION</a></li>
+                                <li><a href="#">SEGUROS</a></li>
+                                <li><a href="#">PERSONAL FIJO</a></li>
+                                <li><a href="#">INSTALACION DE GPS</a></li>
+                                <li><a href="#">CURSO ONLINE Y PRESENCIAL</a></li>
+                                <li><a href="#">IMAGEN CORPORATIVA</a></li>
+                                <li><a href="#">FINANCIAMIENTO EMPRESARIALES</a></li>
+                                <li><a href="#">TRANSPORTACION EMPRESARIAL</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="about.html">BENEFICIOS</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">PLANES PYMES <b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">CONTABLE + FISCAL</a></li>
+                                <li><a href="#">CONTABLE + FISCAL + SOFTWARE</a></li>
+                                <li><a href="#">REDES SOCIALES</a></li>
+                                <li><a href="#">PÁGINAS WEB</a></li>
+                                <li><a href="#">AUDITORIA</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">CONTACTENOS</a></li>
+                        <li class="dropdown hidden">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">PAGES <b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="blog.html">BLOG</a></li>
+                                <li><a href="single-post.html">SINGLE POST</a></li>
+                                <li><a href="portfolio.html">PORTFOLIO</a></li>
+                                <li><a href="single-project.html">SINGLE PROJECT</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
+        </div>
+
+        <!-- *****************************************************************************************************************
+         HEADERWRAP
+         ***************************************************************************************************************** -->
+        <div id="headerwrap">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 ">
+                        <h1>Planes Diseñados</h1>
+                        <h3>Según el tamaño y la necesidad de su empresa o negocio.</h3>
+                    </div>
+                    <div class="col-lg-12 " style="width: 100%;">
+                        <img src="assets/img/portada/ESAPYMES2.jpg" class="img-responsive" width="100%">
+                    </div>
+                </div><!-- /row -->
+            </div> <!-- /container -->
+        </div><!-- /headerwrap -->
+
+        <!-- *****************************************************************************************************************
+         SERVICE LOGOS
+         ***************************************************************************************************************** -->
+        <div id="service">
+            <div class="container">
+                <div class="row centered">
+                    <div class="col-md-6">
+                        <i class="fa fa-building-o"></i>
+                        <h1>Gestión PyMes</h1>
+                        <p>Son servicios novedosos y profesionales, que combina servicios profesionales Contabilidad, impuestos, auditoria, gestión humana, tecnología, marketing, planeación estratégica y legal, mediante planes de servicios especifico por cada una de las áreas, tomando un precio mensual de bajo costo, destinado a las Pequeñas y Medianas Empresas de República Dominicana.</p>
+                    </div>
+                    <div class="col-md-6">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/sJfji_NwAIM" frameborder="0" allowfullscreen></iframe>
+                    </div>		 				
+                </div>
+            </div><! --/container -->
+        </div><! --/service -->
+        <div id="service">
+            <div class="container">
+                <div class="row centered">
+                    <div class="col-md-4">
+                        <i class="fa fa-building-o"></i>
+                        <h4>101 proyectos completados</h4>
+                    </div>
+                    <div class="col-md-4">
+                        <i class="fa fa-smile-o"></i>
+                        <h4>100% Clientes Satisfecho</h4>
+                    </div>
+                    <div class="col-md-4">
+                        <i class="fa fa-users"></i>
+                        <h4>22 empleados</h4>
+                    </div>		 				
+                </div>
+            </div><! --/container -->
+        </div><! --/service -->
+        <div id="service">
+            <div class="container">
+                <h1>Sobre Gestión PyMEs</h1>
+                <div class="row">
+                    <div class="col-md-6">
+                        <p>Desde el 2009, Ges Consultores, SRL, ha estudiado y evaluado como ofrecerle un mayor apoyo profesional y tecnológico a las pequeñas y medianas empresas (pymes) de República Dominicana. Partiendo de nuestro informe del estudio realizado al Ministerio de Industria y Comercio, encontramos que la mayor parte de las empresas pequeñas, no tenían contabilidad organizada, ni controles internos para el control de los recursos de sus empresas. Por ende, no disponen de información financieras para la toma de decisiones.</p>
+                    </div>
+                    <div class="col-md-6">
+                        <p>Nuestros planes están divididos por áreas de servicio, buscando ofrecerle la solución a cada una de las necesidades que se presentan en las pymes. Cada uno de los planes están diseñado, según el tamaño de la empresa. Nuestro servicio ofrece las herramientas para la toma de decisiones eficientes, sin importar su ubicación ni donde se encuentre.</p>
+                        <p>Estos servicios impulsan el crecimiento de su negocio, y lo coloca a las empresas con mayor innovación, disminuyendo costos de inversión en equipos, documentos y personal.</p>
+                    </div>		 				
+                </div>
+            </div><! --/container -->
+        </div><! --/service -->
+
+        <!-- *****************************************************************************************************************
+         OUR CLIENTS
+         ***************************************************************************************************************** -->
+        <div id="cwrap">
+            <div class="container">
+                <div class="row centered">
+                    <h3>Nuestros Clientes</h3>
+                    <div class="col-lg-3 col-md-3 col-sm-3">
+                        <img src="assets/img/clients/client01.png" class="img-responsive">
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-3">
+                        <img src="assets/img/clients/client02.png" class="img-responsive">
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-3">
+                        <img src="assets/img/clients/client03.png" class="img-responsive">
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-3">
+                        <img src="assets/img/clients/client04.png" class="img-responsive">
+                    </div>
+                </div><! --/row -->
+            </div><! --/container -->
+        </div><! --/cwrap -->
+
+        <!-- *****************************************************************************************************************
+         FOOTER
+         ***************************************************************************************************************** -->
+        <div id="footerwrap">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <h4>Acerca de ESA PYMES</h4>
+                        <div class="hline-w"></div>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                    </div>
+                    <div class="col-lg-4">
+                        <h4>Nuestras Redes Sociales:</h4>
+                        <div class="hline-w"></div>
+                        <p>
+                            <a href="#"><i class="fa fa-dribbble"></i></a>
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+                            <a href="#"><i class="fa fa-tumblr"></i></a>
+                        </p>
+                    </div>
+                    <div class="col-lg-4">
+                        <h4>Contáctenos</h4>
+                        <div class="hline-w"></div>
+                        <p>
+                            Some Ave, 987,<br/>
+                            23890, New York,<br/>
+                            United States.<br/>
+                        </p>
+                    </div>
+
+                </div><! --/row -->
+            </div><! --/container -->
+        </div><! --/footerwrap -->
+
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/retina-1.1.0.js"></script>
+        <script src="assets/js/jquery.hoverdir.js"></script>
+        <script src="assets/js/jquery.hoverex.min.js"></script>
+        <script src="assets/js/jquery.prettyPhoto.js"></script>
+        <script src="assets/js/jquery.isotope.min.js"></script>
+        <script src="assets/js/custom.js"></script>
+
+
+        <script>
+            // Portfolio
+            (function ($) {
+                "use strict";
+                var $container = $('.portfolio'),
+                        $items = $container.find('.portfolio-item'),
+                        portfolioLayout = 'fitRows';
+
+                if ($container.hasClass('portfolio-centered')) {
+                    portfolioLayout = 'masonry';
+                }
+
+                $container.isotope({
+                    filter: '*',
+                    animationEngine: 'best-available',
+                    layoutMode: portfolioLayout,
+                    animationOptions: {
+                        duration: 750,
+                        easing: 'linear',
+                        queue: false
+                    },
+                    masonry: {
+                    }
+                }, refreshWaypoints());
+
+                function refreshWaypoints() {
+                    setTimeout(function () {
+                    }, 1000);
+                }
+
+                $('nav.portfolio-filter ul a').on('click', function () {
+                    var selector = $(this).attr('data-filter');
+                    $container.isotope({filter: selector}, refreshWaypoints());
+                    $('nav.portfolio-filter ul a').removeClass('active');
+                    $(this).addClass('active');
+                    return false;
+                });
+
+                function getColumnNumber() {
+                    var winWidth = $(window).width(),
+                            columnNumber = 1;
+
+                    if (winWidth > 1200) {
+                        columnNumber = 5;
+                    } else if (winWidth > 950) {
+                        columnNumber = 4;
+                    } else if (winWidth > 600) {
+                        columnNumber = 3;
+                    } else if (winWidth > 400) {
+                        columnNumber = 2;
+                    } else if (winWidth > 250) {
+                        columnNumber = 1;
+                    }
+                    return columnNumber;
+                }
+
+                function setColumns() {
+                    var winWidth = $(window).width(),
+                            columnNumber = getColumnNumber(),
+                            itemWidth = Math.floor(winWidth / columnNumber);
+
+                    $container.find('.portfolio-item').each(function () {
+                        $(this).css({
+                            width: itemWidth + 'px'
+                        });
+                    });
+                }
+
+                function setPortfolio() {
+                    setColumns();
+                    $container.isotope('reLayout');
+                }
+
+                $container.imagesLoaded(function () {
+                    setPortfolio();
+                });
+
+                $(window).on('resize', function () {
+                    setPortfolio();
+                });
+            })(jQuery);
+        </script>
+    </body>
+</html>
---
+++ ESAPYMES/main.py
@@ -0,0 +1,25 @@
+import os 
+import hashlib
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, g
+from flask import session as secion
+from sqlalchemy import create_engine, or_, desc, asc
+from sqlalchemy.orm import sessionmaker
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+
+app = Flask(__name__)
+app.secret_key = 'super_super_secret'
+app.debug = True
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+from functools import wraps
+
+@app.route('/')
+@app.route('/Welcome')
+def init():
+	return render_template('blog.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
