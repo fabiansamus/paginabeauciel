82673b83986869f11f0765b457fea956576eb0a4
fabiansamus <samus0226@hotmail.com>
2017-07-25 15:11:00 -0400

    hola

--- static/js/user_formulario.js
+++ static/js/user_formulario.js
@@ -1,22 +1,35 @@
-$('.process_customer').click(function(){
-  event.preventDefault();
-  $('form input[name=user_input]').each(function(){
-    var mydata = $(this).val();
-    window.alert(mydata);
-    alert(mydata)
+// $('.process_customer').click(function(){
+//   event.preventDefault();
+//   $('form input[name=user_input]').each(function(){
+//     var mydata = $(this).val();
+//     window.alert(mydata);
+//     alert(mydata)
+//   });
+//   $.ajax({
+//     type: 'POST',
+//     url: '/add_user',
+//     data: mydata,
+//     cache: false,
+//     success: function(result) {
+//       if(result == "true"){
+
+//         alert("el paciente se grego");
+//       }else{
+//         alert("paciente no agrego");
+//       }
+//     }
+//   });
+// });
+
+$('.process_customer').bind('click', function() {
+  $('form input[name=user_input]').each(function (){
+      var myinfo = $(this).val();
+    })
+  $.getJSON('/_add_numbers', {
+    a: myinfo,
+    b: 'test'
+  }, function(data) {
+    $("#result").text(data.result);
   });
-  $.ajax({
-    type: 'POST',
-    url: '/add_user',
-    data: mydata,
-    cache: false,
-    success: function(result) {
-      if(result == "true"){
-        
-        alert("el paciente se grego");
-      }else{
-        alert("paciente no agrego");
-      }
-    }
-  });
+  return false;
 });
